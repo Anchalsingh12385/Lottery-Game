d4c7db1ba149b09ebfec7f875722ae90ecc4c75c
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-05-26 19:12:54 +0530

    updated lottery.jsx

--- lottery-game/src/Lottery.jsx
+++ lottery-game/src/Lottery.jsx
@@ -3,16 +3,16 @@ import "./Lottery.css";
 import { genTicket, sum } from "./helper";
 
 
-export default function Lottery(){
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === 15; // Example winning condition
+export default function Lottery({n,winningSum}){
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket) === winningSum; // Example winning condition
     return(
         <div>
             <h1>Lottery Game</h1>
             <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
+                {ticket.map((num, index) => (
+                    <span key={index}>{num}</span>
+                ))}
             </div>
             <button onClick={() => setTicket(genTicket(3))}>Generate Ticket</button>
             {isWinning && <p>Congratulations! You won!</p>}
